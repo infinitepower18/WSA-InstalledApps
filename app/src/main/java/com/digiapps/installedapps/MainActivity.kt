c8ae94902b87428fe5739ec420fc9907f28203d8
Ahnaf Mahmud <44692189+infinitepower18@users.noreply.github.com>
2022-11-17 20:58:25 +0000

    Update MainActivity.kt

--- app/src/main/java/com/digiapps/installedapps/MainActivity.kt
+++ app/src/main/java/com/digiapps/installedapps/MainActivity.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
