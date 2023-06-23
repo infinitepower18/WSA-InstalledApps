332c60acf9c175de318fbffa8880a783bffe6924
Syuugo <pub@s1204.me>
2023-06-23 22:28:02 +0900

    Fix of WSA

--- app/src/main/java/com/digiapps/installedapps/MainActivity.java
+++ app/src/main/java/com/digiapps/installedapps/MainActivity.java
@@ -3,12 +3,11 @@ package com.digiapps.installedapps;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.provider.Settings;
 
 public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         finishAndRemoveTask();
-        startActivity(new Intent(Settings.ACTION_APPLICATION_SETTINGS));
+        startActivity(new Intent(Intent.ACTION_MAIN).setClassName("com.android.settings", "com.android.settings.applications.ManageApplications"));
     }
 }
